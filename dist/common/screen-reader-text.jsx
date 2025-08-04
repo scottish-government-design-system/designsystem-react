a7407eb9eb291138d30128e9bb3e15df41a35176
Jonathan Sutcliffe <jonathan.sutcliffe@gov.scot>
2025-08-04 08:52:50 +0100

    Delete dist/common/screen-reader-text.jsx

--- dist/common/screen-reader-text.jsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ScreenReaderText = ({ children, ...props }) => {
-    return (<span className="visually-hidden" {...props}>
-            {children}
-        </span>);
-};
-ScreenReaderText.displayName = 'ScreenReaderText';
-exports.default = ScreenReaderText;
