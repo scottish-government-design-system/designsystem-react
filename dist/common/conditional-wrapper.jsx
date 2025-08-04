1f4d421de3c0eb8f26a7744aabd8d1d434363d16
Jonathan Sutcliffe <jonathan.sutcliffe@gov.scot>
2025-08-04 08:53:26 +0100

    Delete dist/common/conditional-wrapper.jsx

--- dist/common/conditional-wrapper.jsx
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * Wraps all children in a specified HTML tag if a condition is met.
- */
-const ConditionalWrapper = ({ condition, wrapper, children }) => condition ? wrapper(children) : children;
-ConditionalWrapper.displayName = 'ConditionalWrapper';
-exports.default = ConditionalWrapper;
